753693074675169dd7031f7187c3f6b4a817f2e5
Branislav Beno <brano.beno@gmail.com>
2020-03-09 00:07:31 +0100

    Removed obsolete cli scripts

--- RunProgress.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem run implementation progress gathering
-java -Xms1G -Xmx4G -jar ImplProgress-R1.1.0 Beta1-all.jar
-pause
--- RunProgress.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-## run implementation progress gathering
-java -jar ImplProgress-R0.3.0-all.jar
